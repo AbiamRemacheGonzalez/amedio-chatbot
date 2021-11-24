6b263f9c9a765d994aea40957f3a5599801b702f
Exulonk <abiamrg.14@gmail.com>
2021-11-24 12:05:40 +0000

    Problemas solucionados, listo para empezar el formulario de compra

--- actions/actions.py
+++ actions/actions.py
@@ -3,6 +3,15 @@ from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
+class ActionResetCompraSlots(Action):
+
+    def name(self) -> Text:
+        return "action_reset_comprar_slots"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        return [SlotSet("familia_procesador", None),SlotSet("modelo_procesador", None)]
 
 class ActionAyudaAdicional(Action):
 
@@ -26,24 +35,23 @@ class ActionAyudaAdicional(Action):
 
         return []
 
-class ActionPedirInformacionSobreReparaciones(Action):
-
+class ActionAskModeloProcesador(Action):
     def name(self) -> Text:
-        return "action_modelo_procesador"
+        return "action_ask_modelo_procesador"
 
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        # button_resp=[{"title":"Horario","payload":'horario'},
-        #     {"title":"Contacto","payload":"contacto"},
-        #     {"title":"Dirección","payload":"ubicacion"},
-        #     {"title":"🔧¿Que reparamos?🔧","payload":"reparación"},
-        #     {"title":"Comprar equipo","payload":"comprar equipo"},
-        #     {"title":"Consultas","payload":"consultas"},
-        #     {"title":"Estado de reparación","payload":"estado de reparación"},
-        #     {"title":"Otros","payload":"otros"},
-        #     {"title":"Salir","payload":"adios"}]
-
-        dispatcher.utter_message(text="Pregunta del modelo de procesador")
-
+        familia_procesador = tracker.get_slot("familia_procesador")
+        if familia_procesador == "amd":
+            button_resp=[{"title":"AMD ryzen 3","payload":'ryzen 3'},
+            {"title":"AMD ryzen 5","payload":"ryzen 5"},
+            {"title":"AMD ryzen 7","payload":"ryzen 7"},
+            {"title":"AMD ryzen 9","payload":"ryzen 9"}]
+        if familia_procesador == "intel":
+            button_resp=[{"title":"Intel i3","payload":'i3'},
+            {"title":"Intel i5","payload":"i5"},
+            {"title":"Intel i7","payload":"i7"},
+            {"title":"Intel i9","payload":"i9"}]
+        dispatcher.utter_message(text="¿Que modelo de procesador quieres?", buttons=button_resp)
         return []
